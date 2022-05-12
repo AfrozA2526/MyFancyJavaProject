530eec8d7d478c7df68234cb553c735573fdf651
AfroZa Afrin <afroza2526@gmail.com>
2022-05-12 16:37:06 +0600

    Tasha's Modified First Commit of May11

--- HelloGit.java
+++ HelloGit.java
@@ -2,7 +2,14 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Printing some numbers");
         for(int i=0; i<4; i++){
-            System.out.println("Serial numbers : "+i);
+            System.out.println("Serial numbers : "+i);			
         }
+	   System.out.println("The numbers are from 0 to 3");
+	   
+	   for(int j=0; j<7; j=j+1){
+            System.out.println("Even numbers : "+j);			
+        }
+	   System.out.println("Even numbers are from 0 to 6");
     }
 }
+
