452c8f3e4bccb7baa9bced2e8eefb4b3116a8621
AfroZa Afrin <afroza2526@gmail.com>
2022-05-12 15:41:12 +0600

    Tasha's First Commit

---
+++ HelloGit.java
@@ -0,0 +1,8 @@
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Printing some numbers");
+        for(int i=0; i<4; i++){
+            System.out.println("Serial numbers : "+i);
+        }
+    }
+}
